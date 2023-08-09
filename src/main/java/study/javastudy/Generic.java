9eb25f2148d0a9f0494b81214ff96f2b5caedf2c
KimGeonWoo <ikkw2040@naver.com>
2023-08-09 13:43:37 +0900

    Generic

---
+++ src/main/java/study/javastudy/Generic.java
@@ -0,0 +1,30 @@
+package study.javastudy;
+
+class EmployeeInfo{
+    public int rank;
+    EmployeeInfo(int rank){
+        this.rank = rank;
+    }
+}
+
+class Person<T,S>{
+    public T info;
+    public S id;
+    Person(T info, S id){
+        this.info = info;
+        this.id = id;
+    }
+
+    public<U> void printInfo(U info){
+        System.out.println(info);
+    }
+}
+public class Generic {
+    public static void main(String[] args) {
+        EmployeeInfo e = new EmployeeInfo(1);
+        Integer i = Integer.valueOf(10);
+//        Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(e, i);
+        Person p1 = new Person(e, i);
+        p1.printInfo(e);
+    }
+}
---
+++ src/main/java/study/javastudy/Generic02.java
@@ -0,0 +1,29 @@
+package study.javastudy;
+
+abstract class Info{
+    public abstract int getLevel();
+}
+
+class EmployeeInfo2 extends Info{
+    public int rank;
+    EmployeeInfo2(int rank){
+        this.rank = rank;
+    }
+    public int getLevel(){
+        return this.rank;
+    }
+}
+
+class Person2<T extends Info>{
+    public T info;
+    Person2(T info){
+        this.info = info;
+    }
+}
+
+public class Generic02 {
+    public static void main(String[] args) {
+        Person2<EmployeeInfo2> p1 = new Person2<EmployeeInfo2>(new EmployeeInfo2(2));
+        Person2<String> p2 = new Person2<String>("부장");//String은 info의 자식이 아니라서 컴파일에러
+    }
+}
