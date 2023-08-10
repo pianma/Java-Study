937b8c0cebdd7f5931aaaa86a2ef1f8bbf1492c8
KimGeonWoo <ikkw2040@naver.com>
2023-08-11 08:53:35 +0900

    Time

---
+++ src/main/java/study/javastudy/TimeEx.java
@@ -0,0 +1,29 @@
+package study.javastudy;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class TimeEx {
+
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+        LocalDate localDate = LocalDate.of(2012, Month.APRIL, 12);
+        System.out.println(localDate);
+
+        LocalTime localTime = LocalTime.of(17, 18);
+        System.out.println(localTime);
+
+        LocalTime localTime2 = LocalTime.parse("10:12:21");
+        System.out.println(localTime2);
+
+        LocalDate theDate = localDateTime.toLocalDate();
+        System.out.println(theDate);
+        Month month = localDateTime.getMonth();
+        System.out.println(month);
+        System.out.println(month.getValue());
+    }
+}
