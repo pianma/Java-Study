25294b64a23a90a1bb53f8cb1ea24710fc68c95f
KimGeonWoo <ikkw2040@naver.com>
2023-08-22 20:37:53 +0900

    Lambda

---
+++ src/main/java/study/javastudy/CompareEx.java
@@ -0,0 +1,15 @@
+package study.javastudy;
+
+public class CompareEx {
+    public static void exec(LambdaEx lambdaEx){
+        int k = 10;
+        int m = 20;
+        int value = lambdaEx.compareTo(k,m);
+    }
+
+    public static void main(String[] args) {
+        exec((i,j)->{
+            return i-j;
+        });
+    }
+}
---
+++ src/main/java/study/javastudy/LambdaEx.java
@@ -0,0 +1,6 @@
+package study.javastudy;
+
+public interface LambdaEx {
+
+    public int compareTo(int value1, int value2);
+}
