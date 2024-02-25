f6bb1eda201e31add54f47f8afb01f0f2fc4ba69
KimGeonWoo <ikkw2040@naver.com>
2024-02-25 22:51:17 +0900

    Inheritance

---
+++ src/main/java/study/javastudy/inheritance/Student.java
@@ -0,0 +1,24 @@
+package study.javastudy.inheritance;
+
+public class Student extends Person{
+    private String college;
+
+    private String major;
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+}
---
+++ src/main/java/study/javastudy/inheritance/StudentRunner.java
@@ -0,0 +1,10 @@
+package study.javastudy.inheritance;
+
+public class StudentRunner {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.getAge();
+    }
+
+}
