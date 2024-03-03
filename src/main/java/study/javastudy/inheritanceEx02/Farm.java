fee7b56e01ba251009fe7b888ecd74e555099f8b
KimGeonWoo <ikkw2040@naver.com>
2024-03-03 20:41:39 +0900

    주석처리

--- src/main/java/study/javastudy/inheritanceEx02/Farm.java
+++ src/main/java/study/javastudy/inheritanceEx02/Farm.java
@@ -9,18 +9,18 @@ public class Farm {
         Pet pet = new Dog();
         pet.Groom();
 
-        System.out.println(pet instanceof  Pet);
-        System.out.println(pet instanceof  Dog);
-        System.out.println(pet instanceof String); //Error
-        System.out.println(pet instanceof Animal);
-        System.out.println(pet instanceof Object);
-
-
-        Animal animal = new Animal();
-
-        System.out.println(animal instanceof pet); //error
-        System.out.println(animal instanceof dog); //error
-        System.out.println(Dog instanceof Animal); //error
-        System.out.println(Pet instanceof Pet);; //error
+//        System.out.println(pet instanceof  Pet);
+//        System.out.println(pet instanceof  Dog);
+//        System.out.println(pet instanceof String); //Error
+//        System.out.println(pet instanceof Animal);
+//        System.out.println(pet instanceof Object);
+//
+//
+//        Animal animal = new Animal();
+//
+//        System.out.println(animal instanceof pet); //error
+//        System.out.println(animal instanceof dog); //error
+//        System.out.println(Dog instanceof Animal); //error
+//        System.out.println(Pet instanceof Pet);; //error
     }
 }
